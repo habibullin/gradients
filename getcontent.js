43a5d60a615776d8789d26f10f1ff0c87d8f2b3e
habibullin <rxletter@gmail.com>
2017-03-29 03:34:56 +0300

    styling

---
+++ getcontent.js
@@ -0,0 +1,15 @@
+$.getJSON( "gradients.json", function( data ) {
+	var items = [];
+	$.each( data, function( name, color ) {
+		items.push( 
+			"<div class='gradient' style='background: linear-gradient(to top, " + color[0] + ", " + color[1] + ");'>"
+				+ name +
+			"</div>"
+		 );
+	});
+ 
+	$( "<div/>", {
+		"class": "gradients",
+		html: items.join( "" )
+	}).appendTo( "body" );
+});
